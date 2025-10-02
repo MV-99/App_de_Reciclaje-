4ec80c029f082387e8e54cf43469152f9e9ff7b9
MV-99 <maribithvallecillo@gmail.com>
2025-10-02 22:56:49 +0000

    changes

--- appReciclaje/src/App.jsx
+++ appReciclaje/src/App.jsx
@@ -2,10 +2,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import RecyclingIcon from '@mui/icons-material/Recycling';
 import './App.css'
 
 
-//function circulo
+//function circulo (aca mandar a llamar el icono)
 function Circulo({ size = 80, color = 'green' }) {
   return (
     <div
@@ -46,33 +47,41 @@ function App() {
         }}
       >
         <div style={{ marginTop: '30px' }}>
+        
           <Circulo size={80} color="#2fb166" />
           <h2 style={{ fontSize: '30px', marginTop: '10px' }}>¡Bienvenido a EcoPoints!</h2>
-          <h4 style={{ marginTop: '-20px' }}>Ingresa tus credenciales</h4>
+          <h4 style={{ marginTop: '-20px'}}>Ingresa tus credenciales</h4>
           <div style={{ marginTop: '90px' }}>
-            <h5 style={{marginTop:'-50px'}}>Usuario</h5>
-            <input placeholder='User123@example.com'
+            <h5 style={{marginTop:'-50px', marginRight:'350px'}}>Usuario</h5>
+            <input placeholder='   User123@example.com'
               type='text'
               style={{
                 width: '450px',
                 height: '45px',
-                borderRadius: '10px',
-                marginTop:'-40px' //altura input usuario
+                borderRadius: '17px',
+                marginTop:'-40px', //altura input usuario
+                fontSize:'12px',
+                borderColor:'#2fb166',
+
               }} />
-              <h5 style={{marginTop:'10px'}}>Contraseña</h5>
-               <input placeholder='User123@example.com'
+              <h5 style={{marginTop:'10px', marginRight:'350px'}}>Contraseña</h5>
+               <input placeholder='    Ingresa tu contraseña'
               type='text'
               style={{
                 width: '450px',
                 height: '45px',
-                borderRadius: '10px',
-                marginTop:'-25px'
+                borderRadius: '17px',
+                marginTop:'-25px',
+                fontSize:'12px',
+                borderColor:'#2fb166'
               }} />
-              <h5 style={{marginTop:'15px'}}>¿No tienes una  cuenta? Registrate</h5>
+              <h5 style={{marginTop:'15px'}}>¿No tienes una  cuenta? <a style={{color:'#2fb166'}}>Registrate</a> </h5>
               <button style={{width:'460px',
               backgroundColor:'#2fb166',
               color:'white',
-              borderRadius:'10px'
+              borderRadius:'17px',
+              fontSize:'15px',
+              height:'45px',
 
               }}>Iniciar Sesion</button>
 
