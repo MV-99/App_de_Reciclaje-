b83e2316b0440a90d818031e588725d323f6e8a2
silvanaTorres21 <85895463+silvanaTorres21@users.noreply.github.com>
2025-10-03 20:59:51 +0000

    changes

--- appReciclaje/src/App.jsx
+++ appReciclaje/src/App.jsx
@@ -6,31 +6,38 @@ import './App.css'
 
 function App() {
   const data = [
-    {titulo: "Puntos totales", valor: "750", desc: "Próxima recompensa:", extra: "1500 pts"},
-    {titulo: "Valor en dinero", valor: "$75.00", desc: "1 punto = $0.10"},
-    {titulo: "Materiales reciclados", valor: "4", desc: "Este mes"}
+    { titulo: "Puntos totales:", valor: "0", desc: "Próxima recompensa:", extra: "1500 pts" },
+    { titulo: "Valor en dinero:", valor: "$0.00", desc: "1 punto = $0.10" },
+    { titulo: "Materiales reciclados:", valor: "0", desc: "Este mes." }
   ];
 
   return (
     <>
-      
+
       <div className="cards-container">
-        {data.map((item, index) =>(
+        {data.map((item, index) => (
           <div key={index} className="card">
             <h3>{item.titulo}</h3>
             <p className="valor">{item.valor}</p>
             <p className="desc">{item.desc} {item.extra && <span>{item.extra}</span>}
             </p>
+            <section id="nivel-container">
+              <div id="barra-nivel">
+                <div id="progreso-nivel"></div>
+              </div>
+            </section>
           </div>
+
         ))}
       </div>
+      
+      <button type="submit">Registrar reciclaje</button>
+      <div className="espacio">
 
-      <div className= "espacio">
-        
       </div>
-     
 
-    
+
+
     </>
 
 
