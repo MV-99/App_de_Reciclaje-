ffb4823e8abd8b72fc9798e5dc9f344d7e0d1a06
MV-99 <maribithvallecillo@gmail.com>
2025-10-03 16:27:53 +0000

    validation login email and password

--- appReciclaje/src/App.jsx
+++ appReciclaje/src/App.jsx
@@ -35,6 +35,25 @@ function Input(placeholder, type) {
 }
 
 function App() {
+  //constantes de input de usuario(email)
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const maxLength = 40; //cantidad maxima de caracteres 
+  const maxLength2 = 16; //cantidad maxima de caracteres en password
+
+  //validacion de error de input email
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+    if (value.length > maxLength) {
+      setEmailError(`Máximo ${maxLength} caracteres`);
+    } else if (!/^\S+@\S+\.\S+$/.test(value)) {
+      setEmailError("Debe ser un email válido");
+    } else {
+      setEmailError("");
+    }
+  };
+
   return (
     <div>
       <div
@@ -47,44 +66,66 @@ function App() {
         }}
       >
         <div style={{ marginTop: '30px' }}>
-        
           <Circulo size={80} color="#2fb166" />
           <h2 style={{ fontSize: '30px', marginTop: '10px' }}>¡Bienvenido a EcoPoints!</h2>
-          <h4 style={{ marginTop: '-20px'}}>Ingresa tus credenciales</h4>
+          <h4 style={{ marginTop: '-20px', fontWeight: 'normal' }}>Ingresa tus credenciales</h4>
           <div style={{ marginTop: '90px' }}>
-            <h5 style={{marginTop:'-50px', marginRight:'350px'}}>Usuario</h5>
-            <input placeholder='   User123@example.com'
-              type='text'
+            <h5 style={{ marginTop: '-50px', marginRight: '350px' }}>Usuario</h5>
+            <input
+              placeholder="   User123@example.com"
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              maxLength={maxLength}
               style={{
                 width: '450px',
                 height: '45px',
                 borderRadius: '17px',
-                marginTop:'-40px', //altura input usuario
-                fontSize:'12px',
-                borderColor:'#2fb166',
-
-              }} />
-              <h5 style={{marginTop:'10px', marginRight:'350px'}}>Contraseña</h5>
-               <input placeholder='    Ingresa tu contraseña'
-              type='text'
+                marginTop: '-40px',
+                fontSize: '12px',
+                borderColor: '#2fb166',
+                border: '2px solid #72b874ff',
+                outline: 'none',
+              }}
+            />
+            {emailError && (
+              <div style={{ color: 'red', fontSize: '12px', marginTop: '2px', marginBottom: '8px' }}>{emailError}</div>
+            )}
+            <h5 style={{ marginTop: '10px', marginRight: '330px' }}>  Contraseña</h5>
+            <input
+              placeholder="    Ingresa tu contraseña"
+              type="password"  //tipo de input (aprender los diversos tipos de input que hay)
+              maxLength={maxLength2}
               style={{
                 width: '450px',
                 height: '45px',
                 borderRadius: '17px',
-                marginTop:'-25px',
-                fontSize:'12px',
-                borderColor:'#2fb166'
-              }} />
-              <h5 style={{marginTop:'15px'}}>¿No tienes una  cuenta? <a style={{color:'#2fb166'}}>Registrate</a> </h5>
-              <button style={{width:'460px',
-              backgroundColor:'#2fb166',
-              color:'white',
-              borderRadius:'17px',
-              fontSize:'15px',
-              height:'45px',
-
-              }}>Iniciar Sesion</button>
-
+                marginTop: '-25px',
+                fontSize: '12px',
+                borderColor: '#2fb166',
+                border: '2px solid #7bc27eff',
+                outline: 'none',
+              }}
+            />
+            <h5 style={{ marginTop: '15px', fontWeight: 'normal' }}>
+              ¿No tienes una  cuenta? <a style={{ color: '#2fb166', fontWeight: 'bold' }}>Registrate</a>
+            </h5>
+            <button
+              style={{
+                width: '480px',
+                backgroundColor: '#2fb166',
+                color: 'white',
+                borderRadius: '17px',
+                fontSize: '15px',
+                height: '45px',
+                outline: 'none',
+                cursor: 'pointer',
+                transition: 'transform 0.6s, backgroundoColor pink  0.8s',
+                transform: 'scale(0.95)',
+              }}
+            >
+              Iniciar Sesion
+            </button>
           </div>
         </div>
       </div>
