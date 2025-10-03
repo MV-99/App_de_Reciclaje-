d2847ca997b1f019ae18268d8124ac6e7cd694b2
silvanaTorres21 <85895463+silvanaTorres21@users.noreply.github.com>
2025-10-03 21:16:01 +0000

    cards

--- appReciclaje/src/App.jsx
+++ appReciclaje/src/App.jsx
@@ -1,47 +1,17 @@
 import { useState } from 'react'
-
+import Cards from './components/cards/cards';
 import './App.css'
 
 
 
 function App() {
-  const data = [
-    { titulo: "Puntos totales:", valor: "0", desc: "Próxima recompensa:", extra: "1500 pts" },
-    { titulo: "Valor en dinero:", valor: "$0.00", desc: "1 punto = $0.10" },
-    { titulo: "Materiales reciclados:", valor: "0", desc: "Este mes." }
-  ];
-
+  
   return (
     <>
-
-      <div className="cards-container">
-        {data.map((item, index) => (
-          <div key={index} className="card">
-            <h3>{item.titulo}</h3>
-            <p className="valor">{item.valor}</p>
-            <p className="desc">{item.desc} {item.extra && <span>{item.extra}</span>}
-            </p>
-            <section id="nivel-container">
-              <div id="barra-nivel">
-                <div id="progreso-nivel"></div>
-              </div>
-            </section>
-          </div>
-
-        ))}
-      </div>
-      
-      <button type="submit">Registrar reciclaje</button>
-      <div className="espacio">
-
-      </div>
-
-
+    <div><Cards/></div>
 
     </>
-
-
-  )
+  );
 }
 
 export default App;
