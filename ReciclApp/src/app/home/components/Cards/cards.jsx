0c39a43c5e6ec85c92d78732c9dfb8fc2ad11c85
silvanaTorres21 <85895463+silvanaTorres21@users.noreply.github.com>
2025-10-07 21:17:54 +0000

    cambios

--- ReciclApp/src/app/home/components/Cards/cards.jsx
+++ ReciclApp/src/app/home/components/Cards/cards.jsx
@@ -11,22 +11,40 @@ function Cards() {
     <>
 
       <div className="cards-container">
-        {data.map((item, index) => (
-          <div key={index} className="card">
-            <h3>{item.titulo}</h3>
-            <p className="valor">{item.valor}</p>
-            <p className="desc">{item.desc} {item.extra && <span>{item.extra}</span>}
-            </p>
-            <section id="nivel-container">
-              <div id="barra-nivel">
-                <div id="progreso-nivel"></div>
-              </div>
-            </section>
-          </div>
+        {data.map((item, index) => {
+          const meta = 1500;
+          let progreso = 0;
+          let faltan = 0;
 
-        ))}
+          if (index === 0) {
+            const points = Number(item.valor) || 0;
+            progreso = Math.min((points / meta) * 100, 100);
+            faltan = meta - points > 0 ? meta - points : 0;
+          }
+          return (
+            <div key={index} className="card">
+              <h3>{item.titulo}</h3>
+              <p className="valor">{item.valor}</p>
+              <p className="desc">{item.desc} {item.extra && <span>{item.extra}</span>}
+              </p>
+
+              {index === 0 && (
+                <section className="nivel-container">
+                  <div className="barra-nivel">
+                    <div 
+                      className="progreso-nivel" 
+                      style={{ width: '${progreso}%' }}>
+                    </div>
+                  </div>
+                  <p className="faltante">Próxima recompensa: {faltan} pts</p>
+                </section>
+              )}
+            </div>
+          );
+        })}
+          
       </div>
-      
+
 
 
     </>
