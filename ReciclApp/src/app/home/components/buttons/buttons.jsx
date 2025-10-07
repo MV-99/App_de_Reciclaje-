711fcae48cd803cb53729bc1d90f44012a89bf74
MV-99 <maribithvallecillo@gmail.com>
2025-10-07 17:23:57 +0000

    :bug: intentando solucionar el bug de que no se ve nada jakakakajajaj

--- ReciclApp/src/app/home/components/buttons/buttons.jsx
+++ ReciclApp/src/app/home/components/buttons/buttons.jsx
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import "@styles/buttons.css";
+=======
+import React, { useState } from "react";
+import "../../../../styles/buttons.css";
+>>>>>>> fea26770c20da85c3a7194f2c905d81232a0e9b0
 import RecyclingIcon from '@mui/icons-material/Recycling';
 import TollIcon from '@mui/icons-material/Toll';
 import ModalRegistrarMaterial from "../modal_reg_material/reg_material";
