a418a153e0c69d61c7de5ffaccb561dffa0265a8
MV-99 <maribithvallecillo@gmail.com>
2025-10-09 20:35:56 +0000

    add modal new

--- ReciclApp/src/app/components/navbar.jsx
+++ ReciclApp/src/app/components/navbar.jsx
@@ -1,6 +1,42 @@
+
+import React, { useState } from "react";
 import "@styles/navbar.css";
+import Modal from 'react-modal';
+import '@styles/modal2.css';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
+
+function handleClick() {
+
+    return (
+        Swal.fire({
+            title: '¡oh no!',
+            text: 'Lo sentimos, aun no esta disponible esta función, se paciente😪',
+            icon: 'error',
+            background: '#ECF7F1',
+            color: '#2FB166',
+            confirmButtonColor: '#2FB166',
+            confirmButtonText: 'Okey',
+            iconColor: '#2FB166',
+            customClass: {
+                confirmButton: 'alert'
+            }
+
+        })
+    );
+}
 
 export default function Navbar() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+   const navigate = useNavigate();
+  function openModal() {
+    setModalIsOpen(true);
+  }
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
   return (
     <nav>
       <div className="logo">
@@ -11,10 +47,19 @@ export default function Navbar() {
         <span>Recicla, gana puntos y ayuda al ambiente</span>
       </div>
       <div className="profile">
-        <a href="">
-          <img src="./profile.png" alt="profile" />
-        </a>
+        <img src='profile.png' alt="profile" onClick={openModal} />
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        className="modalSalir"
+      >
+        <h3 style={{  marginLeft:'40px'}}>⚙️Configuracion</h3>
+         <button className="verPerfil" onClick={handleClick}>Ver perfil</button>
+        <button className="salir"onClick={()=>navigate ('/')}>Cerrar Sesión</button>
+        <button onClick={closeModal} className="cerrar">Cerrar</button>
+      </Modal>
     </nav>
   );
 }
